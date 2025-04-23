c655fdf059b60d77aec04073f989412ba0c658b7
Daniele Tavares Lobo <107322230+danitavareslobo@users.noreply.github.com>
2025-04-23 16:43:05 -0300

    Ex 4 - Operadores lógicos: Salário por hora

--- Trello/Semana3/salario.js
+++ Trello/Semana3/salario.js
@@ -1,18 +1,23 @@
-let nome = "João";
-let setor = "Rh";
+let nome = "Joãolet nome = "João";let nome = "João";
+let setor = "GE";
 let horasTrab = 300;
 let valorHora = 50;
 
-// Horas extras acima de 200h
+// Horas extras acima de 220h;
+// [OP] - sem mudança de cálculo;
+// [AD] - salário base 10% maior;
+// [GE] - salário base 25% maior, mas não possui horas extras;
 
-function salarioHora(pHoras, pValorHora) {
+console.log(salarioHora(horasTrab, valorHora, setor));
+
+function salarioHora(pHoras, pValorHora, pSetor) {
     let horas = 0;
     let horasExtras = 0;
-    
-    if (pHoras > 200) {
-        horas = 200;
-        horasExtras = pHoras - 200;
-    } 
+
+    if (pHoras > 220 && pSetor != "GE") {
+        horas = 220;
+        horasExtras = pHoras - 220;
+    }
     else {
         horas = pHoras;
     }
@@ -20,21 +25,64 @@ function salarioHora(pHoras, pValorHora) {
     return (horas * pValorHora) + (horasExtras * (pValorHora * 1.5));
 }
 
-console.log(salarioHora(horasTrab, valorHora));
-
-console.log(nome + " vc receberá R$ " + 220*valorHora + " referente ao seu salário. Mais R$ " + (horasTrab-220)*(valorHora*1.5) + " referente às suas horas extras. Totalizando assim, um recebimento de R$ " + salarioHora(horasTrab, valorHora) + ".")
 
 
+// console.log(nome + " vc receberá R$ " + 220*valorHora + " referente ao seu salário. Mais R$ " + (horasTrab-220)*(valorHora*1.5) + " referente às suas horas extras, que foram " + (horasTrab-220) + "h. Totalizando assim, um recebimento de R$ " + salarioHora(horasTrab, valorHora) + ".")
+let setor = "GE";
+let horasTrab = 300;
+let valorHora = 50;
+
+// Horas extras acima de 220h;
+// [OP] - sem mudança de cálculo;
+// [AD] - salário base 10% maior;
+// [GE] - salário base 25% maior, mas não possui horas extras;
+
+console.log(salarioHora(horasTrab, valorHora, setor));
+
+function salarioHora(pHoras, pValorHora, pSetor) {
+    let horas = 0;
+    let horasExtras = 0;
+
+    if (pHoras > 220 && pSetor != "GE") {
+        horas = 220;
+        horasExtras = pHoras - 220;
+    }
+    else {
+        horas = pHoras;
+    }
+
+    return (horas * pValorHora) + (horasExtras * (pValorHora * 1.5));
+}
 
 
 
+// console.log(nome + " vc receberá R$ " + 220*valorHora + " referente ao seu salário. Mais R$ " + (horasTrab-220)*(valorHora*1.5) + " referente às suas horas extras, que foram " + (horasTrab-220) + "h. Totalizando assim, um recebimento de R$ " + salarioHora(horasTrab, valorHora) + ".")
+let setor = "GE";
+let horasTrab = 300;
+let valorHora = 50;
+
+// Horas extras acima de 220h;
+// [OP] - sem mudança de cálculo;
+// [AD] - salário base 10% maior;
+// [GE] - salário base 25% maior, mas não possui horas extras;
+
+console.log(salarioHora(horasTrab, valorHora, setor));
+
+function salarioHora(pHoras, pValorHora, pSetor) {
+    let horas = 0;
+    let horasExtras = 0;
+
+    if (pHoras > 220 && pSetor != "GE") {
+        horas = 220;
+        horasExtras = pHoras - 220;
+    }
+    else {
+        horas = pHoras;
+    }
+
+    return (horas * pValorHora) + (horasExtras * (pValorHora * 1.5));
+}
 
 
 
-
-
-
-
-
-
-
+// console.log(nome + " vc receberá R$ " + 220*valorHora + " referente ao seu salário. Mais R$ " + (horasTrab-220)*(valorHora*1.5) + " referente às suas horas extras, que foram " + (horasTrab-220) + "h. Totalizando assim, um recebimento de R$ " + salarioHora(horasTrab, valorHora) + ".")
