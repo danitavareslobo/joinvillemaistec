77dc5f526b602cd497b1a1b1a9f8ff73a7c50492
Daniele Tavares Lobo <107322230+danitavareslobo@users.noreply.github.com>
2025-04-23 10:48:22 -0300

    Ex 2 - Operadores Aritméticos: Salário por hora

--- Trello/Semana3/salario.js
+++ Trello/Semana3/salario.js
@@ -1,8 +1,26 @@
 let nome = "João";
 let setor = "Rh";
-let horasTrab = 100;
+let horasTrab = 300;
 let valorHora = 50;
 
+// Horas extras acima de 200h
+
+function salarioHora(pHoras, pValorHora) {
+    let horas = 0;
+    let horasExtras = 0;
+    
+    if (pHoras > 200) {
+        horas = 200;
+        horasExtras = pHoras - 200;
+    } 
+    else {
+        horas = pHoras;
+    }
+
+    return (horas * pValorHora) + (horasExtras * (pValorHora * 1.5));
+}
+
+console.log(salarioHora(horasTrab, valorHora));
 
 
 
