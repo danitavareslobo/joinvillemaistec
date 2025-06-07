240f63bd93b239e1b0ff1443d768460ce1368f96
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2025-06-07 09:40:42 -0300

    Ex 1 – Loja de plantas: Estrutura do projeto

---
+++ Trello/semana10/lojaPlantas/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>🌱 Loja de Plantas</h1>
+        <p>E-commerce de Plantas de Jardim</p>
+      </header>
+      <main>
+        <p>Projeto inicializado com sucesso!</p>
+        <p>Estrutura do Atomic Design criada e pronta para desenvolvimento.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
